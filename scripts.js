4666c3e7d5ede26f4ce9fb0642d207f14325d06c
Ari Hu <ariwho1@gmail.com>
2024-07-02 10:30:26 -0500

    Refactored Code

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
+// const socket = io();
 const keys = document.querySelectorAll('.piano-key');
 
-const pianoSound = new Audio('./piano-mp3/C4.mp3');
+// const pianoSound = new Audio('./piano-mp3/C4.mp3');
 
 keys.forEach((key) => {
     key.addEventListener('mousedown', (e) => {
         const pressedKey = key.dataset.note;
         console.log("Pressed Piano Note: " + pressedKey);
-        pianoSound.src = `./piano-mp3/${pressedKey}.mp3`;
-        pianoSound.play();
-    })
-})
+        playSound(pressedKey);
+    });
+});
+
+function playSound(note){
+    const audio = new Audio(`./piano-mp3/${note}.mp3`)
+    audio.play();
+}
