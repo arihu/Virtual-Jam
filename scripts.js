cccab519ce14a20a18ceb859f8eee9de02c9c6ba
Ari Hu <ariwho1@gmail.com>
2024-07-02 16:43:22 -0500

    Added Backend Functionality

--- scripts.js
+++ scripts.js
@@ -1,17 +1,36 @@
-// const socket = io();
+const socket = io('http://localhost:3000', {transports: ['websocket']});
+
+socket.on('connect', () =>{
+    console.log("Connected to backend");
+});
+
+socket.on('disconnect', () => {
+    console.log("Disconnected from backend");
+});
+
+
 const keys = document.querySelectorAll('.piano-key');
 
-// const pianoSound = new Audio('./piano-mp3/C4.mp3');
+const Instrument = Object.freeze({
+    Piano: 0,
+})
 
 keys.forEach((key) => {
     key.addEventListener('mousedown', (e) => {
         const pressedKey = key.dataset.note;
         console.log("Pressed Piano Note: " + pressedKey);
-        playSound(pressedKey);
+        playSound(pressedKey, Instrument.Piano);
+        socket.emit('notePress', {note: pressedKey, instrument: Instrument.Piano});
     });
 });
 
-function playSound(note){
-    const audio = new Audio(`./piano-mp3/${note}.mp3`)
+socket.on('notePress', data => {
+    console.log("Received Note from server: " + data.note + ", " + data.instrument);
+    playSound(data.note, data.instrument);
+})
+
+function playSound(note, instrument){
+    if(instrument == Instrument.Piano)
+        audio = new Audio(`./piano-mp3/${note}.mp3`);
     audio.play();
 }
